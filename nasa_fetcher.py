075ec61c5c1f651215d1243674eae96f8b5719ea
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2026-01-30 01:30:41 +0300

    Изменены аргументы функции

--- nasa_fetcher.py
+++ nasa_fetcher.py
@@ -5,7 +5,9 @@ from helpers import get_file_extension
 from helpers import create_img
 
 
-def fetch_nasa_img(nasa_api_key, nasa_api_spare_key, nasa_api_url, folder):      
+def fetch_nasa_img(nasa_api_key, folder):
+    nasa_api_spare_key = 'DEMO_KEY'
+    nasa_api_url = 'https://api.nasa.gov/planetary/apod'
     for api_key, count in [(nasa_api_key, '50'), (nasa_api_spare_key, '5')]:
         try:
             payload = {'api_key': api_key, 'count': count}
@@ -32,9 +34,7 @@ def main():
     os.makedirs(folder, mode=0o755, exist_ok=True)
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_TOKEN']
-    nasa_api_spare_key = 'DEMO_KEY'
-    nasa_api_url = 'https://api.nasa.gov/planetary/apod'
-    fetch_nasa_img(nasa_api_key, nasa_api_spare_key, nasa_api_url, folder)
+    fetch_nasa_img(nasa_api_key, folder)
 
 if __name__ == '__main__':
     main()
