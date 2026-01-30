6b223128609e5179e5b8c936062508c6e472bdca
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2026-01-31 01:48:53 +0300

    Убран лишний сетевой запрос

--- helpers.py
+++ helpers.py
@@ -9,11 +9,11 @@ def get_file_extension(url):
     *others, img_ext = os.path.splitext(full_path)
     return img_ext
 
-def download_image(url, filename, folder):
+def download_image(url, filename, folder, payload=None):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, params=payload, headers=headers)
     response.raise_for_status()
     path = Path(folder) / filename
     with open(path, 'wb') as file:
